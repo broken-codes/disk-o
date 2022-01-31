383bf2557de06fdc74a1e2c4474f424d6161540e
Prashant Chaturvedi <prashant@aiolabs.org>
2022-01-31 07:14:58 +0530

    Initial commit

---
+++ disk-watcher/src/main/java/org/aiolabs/projects/disko/watcher/DiskWatcherApplication.java
@@ -0,0 +1,13 @@
+package org.aiolabs.projects.disko.watcher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiskWatcherApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DiskWatcherApplication.class, args);
+    }
+
+}
---
+++ web-endpoints/src/main/java/org/aiolabs/projects/disko/web/WebEndpointsApplication.java
@@ -0,0 +1,13 @@
+package org.aiolabs.projects.disko.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebEndpointsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebEndpointsApplication.class, args);
+    }
+
+}
